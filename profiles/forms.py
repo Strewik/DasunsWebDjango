e78f3e8a2be40fbc8767a4843f70aea9ea6276c8
Irenyak1 <inyakate@gmail.com>
2021-03-24 23:30:48 +0300

    Add posting booking form and decorators

--- profiles/forms.py
+++ profiles/forms.py
@@ -2,8 +2,8 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-
 from .models import *
+from bootstrap_datepicker_plus import DatePickerInput, TimePickerInput, DateTimePickerInput, MonthPickerInput
 
 class CreateUserForm(UserCreationForm):
 	username = forms.CharField(max_length=254,required=True, widget=forms.TextInput(attrs={'placeholder': 'User Name', 'class': 'form-control'}))
@@ -11,7 +11,6 @@ class CreateUserForm(UserCreationForm):
 	password1 = forms.CharField(max_length=254,required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Create Password', 'class': 'form-control'}))
 	password2 = forms.CharField(max_length=254,required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Confrim Password', 'class': 'form-control'}))
 	
-
 	class Meta:
 		model = User
 		fields = ("username", "email", "password1", "password2")
@@ -23,12 +22,37 @@ class CreateUserForm(UserCreationForm):
 			user.save()
 		return user
 
-
 class ServiceuserForm(ModelForm):
     class Meta:
         model = Serviceuser
         fields = '__all__' 
 
+class BookingForm(ModelForm):
+	class Meta:
+		model = Booking
+		fields = ('name', 'phone', 'email', 'meetplace', 'meetdate','starttime', 'endtime', 'serviceuser', 'serviceprovider')
+		labels  = {
+			'name':'Full Name', 
+			'phone':'Phone No.', 
+			'email':'Email', 
+			'meetplace':'Meeting Place', 
+			'meetdate':'Meeting Date',
+			'starttime':'Start Time',
+			'endtime':'End Time',
+			'serviceuser':'Service User',
+			'serviceprovider':'Service Provider'
+			}
+		widgets = {
+			'name': forms.TextInput(attrs={'placeholder': 'Enter your Full name','class':'form-control'}),
+			'phone': forms.TextInput(attrs={'placeholder': 'Enter your phone number','class':'form-control'}),
+			'email': forms.TextInput(attrs={'placeholder': 'Enter your Email address','class':'form-control'}),
+			'meetplace': forms.TextInput(attrs={'placeholder': 'Preferred meeting place','class':'form-control'}),
+			'meetdate': DatePickerInput(attrs={'placeholder': 'Schedule your preferred date','class':'form-control'}),
+			'starttime': TimePickerInput(attrs={'placeholder': 'Enter Starting time','class':'form-control'}),
+			'endtime': TimePickerInput(attrs={'placeholder': 'Enter Ending time','class':'form-control'}),
+			'serviceuser': forms.Select(attrs={'placeholder': 'Select service user','class':'form-control'}),
+			'serviceprovider': forms.Select(attrs={'placeholder': 'Select service provider','class':'form-control'}),
+		} 
 
 
 # from django import forms
