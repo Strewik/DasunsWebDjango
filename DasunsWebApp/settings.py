6f603271cee1393fd8a932bf205da63518515295
steven <kawooyastevenug@gmail.com>
2021-05-05 12:10:44 +0300

    added signals for serviceuser

--- DasunsWebApp/settings.py
+++ DasunsWebApp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles',
+    'profiles.apps.ProfilesConfig',
     'django_filters',
     'crispy_forms',
     'formtools',
--- profiles/signals.py
+++ profiles/signals.py
@@ -1,3 +1,19 @@
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Serviceuser
+from .models import *
+
+def create_serviceuser(sender,instance,created, **kwargs):
+    if created:
+        Serviceuser.objects.create(user=instance)
+        print("profile created")
+        
+post_save.connect(create_serviceuser, sender=User)
+
+def update_serviceuser(sender,instance, created, **kwargs):
+    if created == False:
+        instance.serviceuser.save()
+        print("profile updated")
+        
+post_save.connect(update_serviceuser, sender=User)          
+        
