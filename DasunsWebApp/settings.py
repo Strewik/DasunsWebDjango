b4722c0d193276998a835745421f2169fb900793
steven <kawooyastevenug@gmail.com>
2021-06-04 02:56:41 +0300

    added gunicorn and locked pip

--- DasunsWebApp/settings.py
+++ DasunsWebApp/settings.py
@@ -94,7 +94,7 @@ TIME_INPUT_FORMATS = ('%H:%M',)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
 
 
     # Connection to Local Database : PostgresQL
