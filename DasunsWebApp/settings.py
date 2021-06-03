543aeafc41e449d61c7194cd136e1a174af2efa4
steven <kawooyastevenug@gmail.com>
2021-06-03 17:50:29 +0300

    configured app to heroku standards

--- DasunsWebApp/settings.py
+++ DasunsWebApp/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "bootstrap4",
     'multiselectfield',      
     'django_cleanup',
+    'herokuapp',
       
 ]
 
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn DasunsWebApp.wsgi --log-file -
