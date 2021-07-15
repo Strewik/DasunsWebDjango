e2785460ca5e97ec8ec5a705041645a93f4180d9
Strewik <52960396+Strewik@users.noreply.github.com>
2021-07-15 13:59:12 +0300

    worked on qualification field

--- DasunsWebApp/settings.py
+++ DasunsWebApp/settings.py
@@ -200,11 +200,7 @@ EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'dasunsdev@gmail.com'
-# EMAIL_HOST_PASSWORD ='asbpgcsbnljiqgpx'
+
 
 
 django_heroku.settings(locals())
