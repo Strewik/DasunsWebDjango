7764a7dbf5d406d55a185c23fb94ad0a121154ae
Irenyak1 <inyakate@gmail.com>
2021-09-02 12:41:17 +0300

    Adds functional email notifications

--- DasunsWebApp/settings.py
+++ DasunsWebApp/settings.py
@@ -201,9 +201,5 @@ EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
-
 
 django_heroku.settings(locals())
